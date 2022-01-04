f4605d628a353bef8e5914064b3a3ec6cc07624c
Amandeep Mewar <96375077+AmandeepMewar@users.noreply.github.com>
2022-01-04 16:48:42 +0530

    Add files via upload

---
+++ infixtopostfix.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define MAX 50
+#include <string.h>
+
+int main(){
+	char str[MAX];
+	printf("Enter a string...\n");
+	scanf("%s",&str);
+	char visited='*';
+	int count,s=0;
+	
+	for (int i=0;i<strlen(str)-1;i++){
+		count=0;
+		for(int j=i+1;j<strlen(str);j++){
+			if (str[j]!=visited){
+				if (str[i]==str[j]){
+					count++;
+					str[j]=visited;
+				}
+			}
+		}
+		if (count>0){
+			s++;
+			if (s==1){
+				printf("%c",str[i]);
+				return 0;
+			}
+		}
+	}
+	printf("Not found\n");
+}
